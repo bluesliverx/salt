e5551b2e0d81ec3513c26c0e387694b78ff2d1a5
saville <saville@adobe.com>
2023-06-13 17:17:10 -0600

    Ignore missing files in spm pkgfile methods

--- tests/pytests/unit/spm/test_pkgfiles.py
+++ tests/pytests/unit/spm/test_pkgfiles.py
@@ -1,10 +1,11 @@
+import hashlib
 import pathlib
 
 import pytest
 
 import salt.spm.pkgfiles.local as spm
 import salt.syspaths
-from tests.support.mock import MagicMock
+from tests.support.mock import MagicMock, patch
 
 
 @pytest.fixture()
@@ -32,3 +33,48 @@ def test_install_file(tmp_path):
         )
         == MockTar().path
     )
+
+
+def test_remove_file_exists(tmp_path):
+    conn = MagicMock()
+    tmp_file = tmp_path / "file1"
+    tmp_file.write_text("text")
+    spm.remove_file(str(tmp_file), conn=conn)
+    assert not tmp_file.exists()
+
+
+def test_remove_file_does_not_exist(tmp_path):
+    conn = MagicMock()
+    tmp_file = tmp_path / "file1"
+    spm.remove_file(str(tmp_file), conn=conn)
+
+
+@patch("salt.spm.pkgfiles.local.os")
+def test_remove_file_raises_exc(os_mock, tmp_path):
+    os_mock.remove.side_effect = OSError("exc")
+    conn = MagicMock()
+    tmp_file = tmp_path / "file1"
+    with pytest.raises(OSError, match="exc"):
+        spm.remove_file(str(tmp_file), conn=conn)
+
+
+def test_hash_file_exists(tmp_path):
+    tmp_file = tmp_path / "file1"
+    tmp_file.write_text("text")
+    hashobj = hashlib.sha1()
+    assert spm.hash_file(str(tmp_file), hashobj) != ""
+
+
+def test_hash_file_does_not_exist(tmp_path):
+    tmp_file = tmp_path / "file1"
+    hashobj = hashlib.sha1()
+    assert spm.hash_file(str(tmp_file), hashobj) == ""
+
+
+def test_hash_file_raises_exc(tmp_path):
+    tmp_file = tmp_path / "file1"
+    tmp_file.touch()
+    hashobj = MagicMock()
+    hashobj.update.side_effect = IOError(0, "exc")
+    with pytest.raises(IOError, match="exc"):
+        spm.hash_file(str(tmp_file), hashobj)
